9663586898aec2a862eaee12c4e78967e1fc7cb2
Saki9140 <sallykirout@gmail.com>
2024-10-21 12:45:43 +0200

    Update Pickpocket.java

--- demo/src/main/java/com/example/Pickpocket.java
+++ demo/src/main/java/com/example/Pickpocket.java
@@ -2,4 +2,8 @@ package com.example;
 
 public class Pickpocket extends Player{
 
+    public Pickpocket(String name, int health, int level, Race race) {
+        super(name, health, level, race);
+        //....
+    }
 }
