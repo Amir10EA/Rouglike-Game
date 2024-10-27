fd64f00bd7b8f538e8bfb19ce6532110b75d3d37
Amir10EA <amirelabidou45@gmail.com>
2024-10-28 00:14:13 +0100

    merged inventory and item branch with the main.

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -9,16 +9,16 @@ public class Main {
         Race race = Race.HUMAN;
         Player player = new Player("Noor", 100, 10, race);
         System.out.println("Player created: " + player.getName());
-        Map<String, Integer> swordMaterials = new HashMap<>();
-        swordMaterials.put("Iron", 2);
+        List<Item> swordMaterials = new ArrayList<>();
+        swordMaterials.add(new Item("Iron", 2, ItemType.SMITHING_STONE));
         Cost swordCost = new Cost(10.0, swordMaterials);
         Weapon sword = new Weapon("Sword", 15, 100, 20, 1.5, WeaponType.SWORD, swordCost);
-        Map<String, Integer> axeMaterials = new HashMap<>();
-        axeMaterials.put("Steel", 3);
+        List<Item> axeMaterials = new ArrayList<>();
+        axeMaterials.add(new Item("Steel", 3, ItemType.SMITHING_STONE));
         Cost axeCost = new Cost(15.0, axeMaterials);
         Weapon axe = new Weapon("Axe", 20, 80, 25, 1.2, WeaponType.AXE, axeCost);
-        player.addWeapon(sword);
-        player.addWeapon(axe);
+        player.addItem(sword);
+        player.addItem(axe);
         System.out.println("Weapons added to inventory.");
         player.equipWeapon(sword);
         System.out.println("Weapon equipped: " + player.getActiveWeapon().getName());
