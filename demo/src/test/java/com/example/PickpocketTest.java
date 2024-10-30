7f734a441a0b7b72bee7161db8bbb5709a6487ff
Saki9140 <sallykirout@gmail.com>
2024-10-30 01:41:56 +0100

    Update PickpocketTest.java

--- demo/src/test/java/com/example/PickpocketTest.java
+++ demo/src/test/java/com/example/PickpocketTest.java
@@ -6,36 +6,161 @@ import org.junit.jupiter.api.Test;
 public class PickpocketTest {
 
     @Test
-    public void testOpenLock() {
-        Pickpocket thief = new Pickpocket("Locke", 100, 1, Race.HUMAN);
-        assertTrue(thief.openLock(2), "Pickpocket borde kunna dyrka upp ett lås med svårighet 2");
+    public void testHumanOpenLock() {
+        Pickpocket humanPickpocket = new Pickpocket("Human", 100, 1, Race.HUMAN);
+        assertTrue(humanPickpocket.openLock(2), "Human Pickpocket should open lock with difficulty 2");
+    }
+
+    @Test
+    public void testElfOpenLock() {
+        Pickpocket elfPickpocket = new Pickpocket("Elf", 100, 1, Race.ELF);
+        assertTrue(elfPickpocket.openLock(2), "Elf Pickpocket should open lock with difficulty 2");
+    }
+
+    @Test
+    public void testGoblinOpenLock() {
+        Pickpocket goblinPickpocket = new Pickpocket("Goblin", 100, 1, Race.GOBLIN);
+        assertTrue(goblinPickpocket.openLock(2), "Human Pickpocket should open lock with difficulty 2");
     }
 
     @Test
     public void testFailToOpenLock() {
-        Pickpocket thief = new Pickpocket("Locke", 100, 1, Race.GOBLIN);
-        assertFalse(thief.openLock(5), "Pickpocket borde inte kunna dyrka upp ett lås med svårighet 5");
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        assertFalse(humanPickpocket.openLock(7), "Human should not be able to open a lock with difficulty 5");
     }
 
     @Test
-    public void testSneakPastEnemy() {
-        Pickpocket thief = new Pickpocket("Locke", 100, 1, Race.ELF);
-        assertTrue(thief.sneakPastEnemy(3), "Pickpocket borde kunna smyga förbi en fiende med vaksamhet 3");
+    public void testMaxLockDifficultyForLevel1PlayerEachRaceCanOpen() {
+        int maxHumanLockDifficulty = 4; 
+        int maxElfLockDifficulty = 5; 
+        int maxGoblinLockDifficulty = 3;
+
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        Pickpocket elfPickpocket = new Pickpocket("elfPickpocket", 100, 1, Race.ELF);
+        Pickpocket goblinPickpocket = new Pickpocket("GoblinPickpocket", 100, 1, Race.GOBLIN);
+
+        assertAll("Check max lock difficulty for level 1 players of each race",
+        () -> assertTrue(humanPickpocket.openLock(maxHumanLockDifficulty),
+            "Human should be able to open a lock with difficulty " + maxHumanLockDifficulty),
+        () -> assertTrue(elfPickpocket.openLock(maxElfLockDifficulty),
+            "Elf should be able to open a lock with difficulty " + maxElfLockDifficulty),
+        () -> assertTrue(goblinPickpocket.openLock(maxGoblinLockDifficulty),
+            "Goblin should be able to open a lock with difficulty " + maxGoblinLockDifficulty)
+    );
     }
 
     @Test
-    public void testPickPocket() {
-        Pickpocket thief = new Pickpocket("Locke", 100, 1, Race.GOBLIN);
-        assertTrue(thief.pickPocket(3), "Pickpocket borde kunna stjäla från NPC med vaksamhet 3");
+    public void testSneakPastEnemyWithAwarenessLevelOne() {
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        Pickpocket elfPickpocket = new Pickpocket("elfPickpocket", 100, 1, Race.ELF);
+        Pickpocket goblinPickpocket = new Pickpocket("GoblinPickpocket", 100, 1, Race.GOBLIN);
+
+        int npcAwareness = 1;
+        
+        assertAll("Check if each race's pickpocket can sneak past an enemy with awareness level " + npcAwareness,
+            () -> assertTrue(humanPickpocket.sneakPastEnemy(npcAwareness), 
+                "Human pickpocket should be able to sneak past an enemy with awareness " + npcAwareness),
+            () -> assertTrue(elfPickpocket.sneakPastEnemy(npcAwareness), 
+                "Elf pickpocket should be able to sneak past an enemy with awareness " + npcAwareness),
+            () -> assertTrue(goblinPickpocket.sneakPastEnemy(npcAwareness), 
+                "Goblin pickpocket should be able to sneak past an enemy with awareness " + npcAwareness)
+        );
     }
 
     @Test
-    public void testLevelUpSkills() {
-        Pickpocket thief = new Pickpocket("Locke", 100, 1, Race.HUMAN);
-        thief.setLevel(2);
-        assertEquals(6, thief.getLockPickingSkill(), "Lockpicking-färdigheten borde öka till 6 vid nivå 2 för Human");
-        assertEquals(7, thief.getSneakingSkill(), "Smygningsfärdigheten borde öka till 7 vid nivå 2 för Human");
+    public void testSneakPastEnemyWithAwarenessLevelFive() {
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        Pickpocket elfPickpocket = new Pickpocket("elfPickpocket", 100, 1, Race.ELF);
+        Pickpocket goblinPickpocket = new Pickpocket("GoblinPickpocket", 100, 1, Race.GOBLIN);
+
+        int npcAwareness = 5;
+        
+        assertAll("Check if each race's pickpocket can sneak past an enemy with awareness level " + npcAwareness,
+            () -> assertFalse(humanPickpocket.sneakPastEnemy(npcAwareness), 
+                "Human pickpocket should not be able to sneak past an enemy with awareness " + npcAwareness),
+            () -> assertTrue(elfPickpocket.sneakPastEnemy(npcAwareness), 
+                "Elf pickpocket should be able to sneak past an enemy with awareness " + npcAwareness),
+            () -> assertFalse(goblinPickpocket.sneakPastEnemy(npcAwareness), 
+                "Goblin pickpocket should not be able to sneak past an enemy with awareness " + npcAwareness)
+        );
     }
+
+    @Test
+    public void testPickPocketing() {
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        Pickpocket elfPickpocket = new Pickpocket("elfPickpocket", 100, 1, Race.ELF);
+        Pickpocket goblinPickpocket = new Pickpocket("GoblinPickpocket", 100, 1, Race.GOBLIN);
+
+        int npcAwareness = 3;
+
+        assertAll("Check if each race's pickpocket can successfully pickpocket with NPC awareness " + npcAwareness,
+            () -> assertTrue(humanPickpocket.pickPocket(npcAwareness), 
+                "Human pickpocket should be able to pickpocket an NPC with awareness " + npcAwareness),
+            () -> assertTrue(elfPickpocket.pickPocket(npcAwareness), 
+                "Elf pickpocket should be able to pickpocket an NPC with awareness " + npcAwareness),
+            () -> assertTrue(goblinPickpocket.pickPocket(npcAwareness), 
+                "Goblin pickpocket should be able to pickpocket an NPC with awareness " + npcAwareness)
+        );
+    }
+
+    @Test
+    public void testLevelUpSkillsForHuman() {
+        Pickpocket humanPickpocket = new Pickpocket("HumanPickpocket", 100, 1, Race.HUMAN);
+        humanPickpocket.setLevel(2);
+
+        int expectedLockPickingSkill = 6;  
+        int expectedSneakingSkill = 7;
+        int expectedPickpocketingSkill = 9;
+
+        assertAll("Level-up skill increases for Human race",
+            () -> assertEquals(expectedLockPickingSkill, humanPickpocket.getLockPickingSkill(), 
+                "Lockpicking skill should increase to 6 at level 2 for Human"),
+            () -> assertEquals(expectedSneakingSkill, humanPickpocket.getSneakingSkill(), 
+                "Sneaking skill should increase to 7 at level 2 for Human"),
+            () -> assertEquals(expectedPickpocketingSkill, humanPickpocket.getPickpocketingSkill(), 
+                "Pickpockting skill should increase to 9 at level 2 for Human")
+        );
+    }
+   
+    @Test
+    public void testLevelUpSkillsForElf() {
+        Pickpocket elfPickpocket = new Pickpocket("ElfPickpocket", 100, 1, Race.ELF);
+        elfPickpocket.setLevel(2);
+
+        int expectedLockPickingSkill = 7;
+        int expectedSneakingSkill = 8;
+        int expectedPickpocketingSkill = 10;
+
+        assertAll("Level-up skill increases for Elf race",
+            () -> assertEquals(expectedLockPickingSkill, elfPickpocket.getLockPickingSkill(), 
+                "Lockpicking skill should increase to 6 at level 2 for Elf"),
+            () -> assertEquals(expectedSneakingSkill, elfPickpocket.getSneakingSkill(), 
+                "Sneaking skill should increase to 9 at level 2 for Elf"),
+            () -> assertEquals(expectedPickpocketingSkill, elfPickpocket.getPickpocketingSkill(), 
+                "Pickpocketing skill should increase to 11 at level 2 for Elf")
+        );
+    }
+
+    @Test
+    public void testLevelUpSkillsForGoblin() {
+        Pickpocket goblinPickpocket = new Pickpocket("GoblinPickpocket", 100, 1, Race.GOBLIN);
+        goblinPickpocket.setLevel(2);
+
+        int expectedLockPickingSkill = 5;
+        int expectedSneakingSkill = 7;
+        int expectedPickpocketingSkill = 9;
+
+        assertAll("Level-up skill increases for Goblin race",
+            () -> assertEquals(expectedLockPickingSkill, goblinPickpocket.getLockPickingSkill(), 
+                "Lockpicking skill should increase to 5 at level 2 for Goblin"),
+            () -> assertEquals(expectedSneakingSkill, goblinPickpocket.getSneakingSkill(), 
+                "Sneaking skill should increase to 8 at level 2 for Goblin"),
+            () -> assertEquals(expectedPickpocketingSkill, goblinPickpocket.getPickpocketingSkill(), 
+                "Pickpocketing skill should increase to 10 at level 2 for Goblin")
+        );
+    }
+
+
 }
 
 
